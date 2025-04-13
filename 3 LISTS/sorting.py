0b79d993bd10321ebb08ba0b767d884a209cca34
vivekjangid17 <vj833930@gmail.com>
2025-04-13 21:04:31 +0530

    add new file sorting

---
+++ 3 LISTS/sorting.py
@@ -0,0 +1,23 @@
+#  Sorting a List Permanently with the sort() Method
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+
+# Sorting a List Temporarily with the sorted() Function
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'Subaru','Bcmw', 'Iaudi', 'HHtoyota', 'ubaru']
+cars.sort()
+print(cars)   #['Bcmw', 'HHtoyota', 'Iaudi', 'Subaru', 'audi', 'bmw', 'toyota', 'ubaru']
+
+#  Printing a List in Reverse Order
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
