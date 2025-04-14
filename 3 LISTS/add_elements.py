2dfec30d956699c5d19393115d475bcc924d5fa7
vivekjangid17 <vj833930@gmail.com>
2025-04-14 13:09:42 +0530

    update file

--- 3 LISTS/add_elements.py
+++ 3 LISTS/add_elements.py
@@ -1,10 +1,7 @@
 # Appending Elements to the End of a List
 
-<<<<<<< HEAD
 friends = ['Ravindra', 'Radha', 'Vivek', 'Ravi']
-=======
 friends = ['Ravindra', 'Radha', 'Vivek']
->>>>>>> 89cabd85a87c57e901f6d7be2a6448e92cecac8a
 print(friends)
 friends.append('Tara')
 print(friends)
