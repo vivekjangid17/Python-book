4ae0ec846f5198fbba43c3f70618330319442fab
vivekjangid17 <vj833930@gmail.com>
2025-04-19 12:42:05 +0530

    add more exercise

---
+++ 4  WORKING WITH LISTS/ex_4.3.py
@@ -0,0 +1,4 @@
+''' Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive. '''
+
+for i in range(1,21):
+    print(i,end=" ")
---
+++ 4  WORKING WITH LISTS/ex_4.4.py
@@ -0,0 +1,6 @@
+''' Make a list of the numbers from one to one million, and then 
+use a for loop to print the numbers. (If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.) '''
+
+lst = list(range(1,1000001))
+for i in lst:
+    print(i)
