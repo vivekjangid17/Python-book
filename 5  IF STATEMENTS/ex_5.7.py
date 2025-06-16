3bd9de83cd5ef86878d49a9e68bd746220bc0177
vivekjangid17 <vj833930@gmail.com>
2025-06-16 15:29:05 +0530

    Reinitialized Git repo after fixing config

---
+++ 5  IF STATEMENTS/ex_5.7.py
@@ -0,0 +1,15 @@
+'''5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+ • Make a list of your three favorite fruits and call it favorite_fruits.
+ • Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block should print a statement, such as You really like bananas!'''
+ 
+favorite_fruits = ["mango","apple","orange"]
+if "mango" in favorite_fruits:
+    print("I really like mango")
+if "banana" in favorite_fruits:
+    print("I really like banana")
+if "apple" in favorite_fruits:
+    print("I really like apple")
+if "papaya" in favorite_fruits:
+    print("I really like papaya")
+if "orange" in favorite_fruits:
+    print("I really like orange")
