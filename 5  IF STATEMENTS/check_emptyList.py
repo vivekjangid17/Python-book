e2d3dd7782238b362c0717470455f8a095c8a3a2
vivekjangid17 <vj833930@gmail.com>
2025-06-16 16:53:24 +0530

    add check empty file

---
+++ 5  IF STATEMENTS/check_emptyList.py
@@ -0,0 +1,17 @@
+# my example
+items = []
+
+for item in items:
+    print(f"Item: {item}")
+else:
+    print("The list is empty, nothing to display.")
+
+#book example
+# requested_toppings = []
+
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print(f"Adding {requested_topping}.")
+# else:
+#     print("Are you sure you want a plain pizza?")
+    
