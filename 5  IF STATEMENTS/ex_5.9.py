c9672b3f2d6c39d2852f5a11802a7bd2c2e310cc
vivekjangid17 <vj833930@gmail.com>
2025-06-19 12:07:07 +0530

    modify code

--- 5  IF STATEMENTS/ex_5.9.py
+++ 5  IF STATEMENTS/ex_5.9.py
@@ -2,15 +2,16 @@
  • If the list is empty, print the message We need to find some users!
  • Remove all of the usernames from your list, and make sure the correct message is printed. '''
  
+
 user_names = ['admin', 'kanak', 'nisha', 'payal', 'devid']
-for user in user_names:
-    print(f'user: {user}')
+
+user_names.clear()  # Clear the list to simulate an empty list
+
+if user_names:
     for user in user_names:
-        user_names.pop()
+        if user == 'admin':
+            print("Hello admin, would you like to see a status report?")
+        else:
+            print(f"Hello {user}, thank you for logging in again.")
 else:
     print("We need to find some users!")
-# for users in user_names:
-#     user_names.remove()
-    
-print(user_names)
-
