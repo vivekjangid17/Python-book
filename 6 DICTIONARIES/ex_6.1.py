57e0b95af648a50195bd3a7f9fd0d08cb8237f9d
Vivek Jangid <154461755+vivekjangid17@users.noreply.github.com>
2025-06-20 15:02:16 +0000

    add new exercise

---
+++ 6 DICTIONARIES/ex_6.1.py
@@ -0,0 +1,5 @@
+'''6-1. Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.'''
+
+person_info = {'first_name': 'Virat', 'last_name': 'Kohli', 'age': 36, 'city': 'Delhi'}
+for info in person_info:
+  print(person_info[info])
