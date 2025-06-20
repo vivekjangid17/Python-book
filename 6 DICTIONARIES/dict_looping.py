c7f76cd7d6c72cae6014a528d6d57fa78b0cbe5a
Vivek Jangid <154461755+vivekjangid17@users.noreply.github.com>
2025-06-20 17:30:48 +0000

    add new file

---
+++ 6 DICTIONARIES/dict_looping.py
@@ -0,0 +1,22 @@
+# Looping Through All the Keys in a Dictionary
+
+favorite_languages = {
+ 'jen': 'python',
+ 'sarah': 'c',
+ 'edward': 'rust',
+ 'phil': 'python',
+ }
+for name in favorite_languages.keys():  # The keys() method is useful when you don’t need to work with all of the values in a dictionary
+    print(name.title())
+print()
+    
+# Looping through the keys is actually the default behavior when looping through a dictionary
+for name in favorite_languages:
+    print(name.title())
+
+friends = ['phil', 'sarah']
+for name in favorite_languages.keys():
+    print(f'Hii, {name.title()}')
+    if name in friends:
+        language = favorite_languages[name].title()
+        print(f'\t{name.title()}, I see you love {language}!')
