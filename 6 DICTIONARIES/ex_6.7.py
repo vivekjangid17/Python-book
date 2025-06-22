f0a94483da1723697ed043b5cc53b0e8b1573513
vivekjangid17 <vj833930@gmail.com>
2025-06-23 03:52:47 +0530

    add new exercise 6.7.py

---
+++ 6 DICTIONARIES/ex_6.7.py
@@ -0,0 +1,7 @@
+'''6-7. People: Start with the program you wrote for Exercise 6-1 (page 98). Make two new dictionaries representing different people, and store all three dictionaries in a list called people. Loop through your list of people. As you loop through the list, print everything you know about each person.'''
+
+person_info1 = {'first_name': 'Virat', 'last_name': 'Kohli', 'age': 36, 'city': 'Delhi'}
+person_info2 = {'first_name': 'Rohit', 'last_name': 'Sharma', 'age': 37, 'city': 'Mumbai'}
+person_info3 = {'first_name': 'Vivek', 'last_name': 'Jangid', 'age': 20, 'city': 'Alwar'}
+person = [person_info1, person_info2, person_info3]
+print(person)
