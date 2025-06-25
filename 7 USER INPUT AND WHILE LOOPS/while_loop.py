1300b23291e6cc6b0512d56a7a3d9284a27ed8ef
vivekjangid17 <vj833930@gmail.com>
2025-06-25 22:18:02 +0530

    add while_loop.py

---
+++ 7 USER INPUT AND WHILE LOOPS/while_loop.py
@@ -0,0 +1,9 @@
+# The for loop takes a collection of items and executes a block of code once for each item in the collection
+
+# The while loop runs as long as, or while, a certain condition is true.
+# while loop counts from 1 to 5:
+i = 1
+while(i<=5):
+    print(i)
+    i+=1
+    
