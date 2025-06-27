f143d3642391133e3b166eccc2b3054ef7c00350
vivekjangid17 <vj833930@gmail.com>
2025-06-27 20:06:47 +0530

    add new exercise

---
+++ 7 USER INPUT AND WHILE LOOPS/ex_7.7.py
@@ -0,0 +1,7 @@
+'''7-7. Infinity: Write a loop that never ends, and run it. (To end the loop, press CTRL-C or close the window displaying the output.)'''
+
+# infinite loop
+i = 1
+while True:
+    print(i)
+    i+=1
