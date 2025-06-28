e226fed21fcd7f0c855f8f452c5ac8b37f2bc129
vivekjangid17 <vj833930@gmail.com>
2025-06-28 07:17:48 +0530

    add file mountain.py to dictionary with while

---
+++ 7 USER INPUT AND WHILE LOOPS/Dictionary with while/mountain_poll.py
@@ -0,0 +1,22 @@
+# Let;s make a polling program in which each pass through the loop prompts for the participant's name and response. We'll store the data we gather in a dictionary, because we want to connect each response with a particular user:
+
+responses = {}
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    # Store the response in the dictionary.
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        polling_active = False
+
+# Polling is complete. Show the results.
+print("\n--- Poll Results ---")
+for name, response in responses.items():
+    print(f"{name.title()} would like to climb {response.title()}.")
