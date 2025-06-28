45b52e431332ddf939c3f6827e854545a823b688
vivekjangid17 <vj833930@gmail.com>
2025-06-28 21:32:22 +0530

    add exercise ex_8.01

---
+++ 8 FUNCTIONS/ex_8.01.py
@@ -0,0 +1,7 @@
+'''8-1. Message: Write a function called display_message() that prints one sentence telling everyone what you are learning about in this chapter. Call the function, and make sure the message displays correctly.'''
+
+def display_message():
+    print("I am learning the Functions in python and a variety of ways to pass information to functions. ")
+
+
+display_message()
