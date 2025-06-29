5a1dd4ac66b3e4396d5ab09cb12e21096d5133e1
vivekjangid17 <vj833930@gmail.com>
2025-06-29 06:13:29 +0530

    add keyword_arguments.py file to passing arguments folder print(f) EOF exit(0) exit ;

---
+++ 8 FUNCTIONS/Passing Arguments/keyword_arguments.py
@@ -0,0 +1,8 @@
+# Keyword Arguments
+
+def full_name(f_name,l_name):
+    print(f'My full is {f_name} {l_name}.')
+
+
+full_name(f_name='Vivek',l_name='Jangid')
+full_name(l_name='Jangid',f_name='Vivek')   # Order does not Matters in Keyword Arguments
