611a51fadc8c947b28dd9fea49ea44f33d90ca34
vivekjangid17 <vj833930@gmail.com>
2025-10-11 20:25:28 +0530

    add new file

---
+++ 8 FUNCTIONS/Passing a List/Preventing_Modification.py
@@ -0,0 +1,30 @@
+# Send a copy of the list instead of the original:
+
+# print_models(unprinted[:], completed)    # at the time of function call
+
+
+# Using functions
+      
+def print_models(unprinted_designs, completed_models):
+    """
+    Simulate printing each design, until none are left.  
+    Move each design to completed_models after printing.
+    """
+    while unprinted_designs:
+        current_design = unprinted_designs.pop()
+        print(f"Printing model: {current_design}")
+        completed_models.append(current_design)
+    
+def show_completed_models(completed_models):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed:")
+    for completed_model in completed_models:
+        print(completed_model)
+        
+unprinted_designs = ['phone case', 'robot pendant', 'dodecahedron']
+completed_models = []
+    
+print_models(unprinted_designs[:], completed_models)
+show_completed_models(completed_models)
+
+print(unprinted_designs)  # original list remains same
