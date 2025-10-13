660e635bf14ccfefa61edc157a87b8e088d07677
vivekjangid17 <vj833930@gmail.com>
2025-10-13 21:59:39 +0530

    add new exercises

---
+++ 8 FUNCTIONS/ex_8.16.py
@@ -0,0 +1,16 @@
+''' 8-16. Imports: Using a program you wrote that has one function in it, store that function in a separate file. Import the function into your main program file, and call the function using each of these approaches:
+ import module_name
+ from module_name import function_name
+ from module_name import function_name as fn
+ import module_name as mn
+ from module_name import *
+ '''
+ 
+# Following are the ways of importing a module, specific functions and all the functions in a python code
+
+# import module_name
+# from module_name import function_name
+# from module_name import function_name as fn
+# import module_name as mn
+# from module_name import *
+
