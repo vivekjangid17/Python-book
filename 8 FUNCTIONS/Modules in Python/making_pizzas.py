243054b8352eec79ffa4c2b2c58484f3a96a0e56
vivekjangid17 <vj833930@gmail.com>
2025-10-13 21:41:42 +0530

    add new file

---
+++ 8 FUNCTIONS/Modules in Python/making_pizzas.py
@@ -0,0 +1,10 @@
+import pizza
+# from pizza import make_pizza
+
+#  Importing All Functions in a Module 
+#  from pizza import *
+# (Because every function is imported, you can call each function by name without using the dot notation.)
+
+pizza.make_pizza(16, 'pepperoni')
+pizza.make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
+# make_pizza(16, 'pepperoni')
