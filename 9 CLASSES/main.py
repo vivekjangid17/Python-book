b4ff39aa163933cb62448b2be444727c9a699cc8
vivekjangid17 <vj833930@gmail.com>
2025-10-13 23:31:24 +0530

    add files to class folder

---
+++ 9 CLASSES/main.py
@@ -0,0 +1,16 @@
+
+class Atm:
+    
+    
+    def __init__(self):      # Constructor - automatically execute when obj of  a class created
+        
+        # self.pin = ''
+        # self.balance = 0
+        
+        # self.menu()
+        print("hello")
+    
+    def menu(self):
+        pass
+        
+obj = Atm()
